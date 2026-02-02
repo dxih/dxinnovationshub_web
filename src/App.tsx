934651ca0e4e1debf8f9102f07dcef608025248f
Mebuge Kamsiyochukwu Brendan <139298439+Brendanmebson@users.noreply.github.com>
2026-02-02 07:57:17 +0100

    Refactor App component to use React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,45 @@
-import { ThemeProvider, CssBaseline, Box, } from '@mui/material';
+import { ThemeProvider, CssBaseline, Box } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 import { theme } from './theme';
+
 import Navbar from './components/NavBar';
-import Hero from "./components/Hero"
-import ServiceCard from "./components/ServiceCard";
-import  WhyDXIH from "./components/WhyDXIH"
+import Hero from './components/Hero';
+import ServiceCard from './components/ServiceCard';
+import WhyDXIH from './components/WhyDXIH';
 import GetStarted from './components/GetStarted';
-import Footer from "./components/Footer"
-
-
-import Portfolio from "./components/Portfolio";
+import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
 
 function App() {
-  
-
   return (
-    <>
     <ThemeProvider theme={theme}>
       <CssBaseline />
+
+      <Navbar />
+
       <Box component="main">
-        <Navbar />
-         <Hero />
-     <ServiceCard />
-     <Portfolio />
-    <WhyDXIH />
-    <GetStarted />
-    < Footer />
+        <Routes>
+          {/* Home / Landing Page */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <ServiceCard />
+                <Portfolio />
+                <WhyDXIH />
+              </>
+            }
+          />
+
+          {/* Get Started Page */}
+          <Route path="/start" element={<GetStarted />} />
+        </Routes>
       </Box>
 
+      <Footer />
     </ThemeProvider>
-   
-
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
